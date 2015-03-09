a3ba2a5d1bf236b023e6beb21c0bf32e37d0c52a
stepheta <stepheta@amazon.com>
2015-03-09 09:41:21 +0000

    Added symlink for pathogen

--- setup.py
+++ setup.py
@@ -3,23 +3,31 @@ import ctypes
 import sys
 import urllib
 
+platform = sys.platform
+
+def symLink(linkTargetPath, linkPath, isDirectory):
+	if platform.startswith('linux'):
+		os.symlink(linkTargetPath, linkPath)
+	elif platform.startswith('win'):
+		kdll = ctypes.windll.LoadLibrary('kernel32.dll')
+		isDirectoryInt = 1 if isDirectory else 0
+		kdll.CreateSymbolicLinkA(linkPath, linkTargetPath, isDirectoryInt)
+
 homepath = os.path.expanduser('~')
 
-vimrcTargetPath = os.getcwd() + '\\.vimrc'
-vimFilesTargetPath = os.getcwd() + '\\.vim'
+vimrcTargetPath = os.getcwd() + '/.vimrc'
+vimFilesTargetPath = os.getcwd() + '/.vim'
 vimrcLinkPath = ''
 vimFilesLinkPath = ''
 
-if sys.platform.startswith('linux'):
+if platform.startswith('linux') or platform.startswith('darwin'):
 	vimrcLinkPath = homepath + '/.vimrc'
 	vimFilesLinkPath = homepath + '/.vim'
-
-	os.symlink(vimrcTargetPath, vimrcLinkPath)
-	os.symlink(vimFilesTargetPath, vimFilesLinkPath)
-elif sys.platform.startswith('win'):
+elif platform.startswith('win'):
 	vimrcLinkPath = homepath + '\\_vimrc'
 	vimFilesLinkPath = homepath + '\\vimfiles'
 
-	kdll = ctypes.windll.LoadLibrary('kernel32.dll')
-	kdll.CreateSymbolicLinkA(vimrcLinkPath, vimrcTargetPath, 0)
-	kdll.CreateSymbolicLinkA(vimFilesLinkPath, vimFilesTargetPath, 1)
+symLink(vimrcTargetPath, vimrcLinkPath, False)
+symLink(vimFilesTargetPath, vimFilesLinkPath, True)
+
+symLink(os.getcwd() + '/vim-pathogen/autoload/pathogen.vim', os.getcwd() + '/.vim/autoload/pathogen.vim', 0)
